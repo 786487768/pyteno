8e66a38a7a27cb5dee04c6a9d1bec5fc3fcc52d2
LinLi <786487768@qq.com>
2017-11-20 01:09:08 -0800

    init Teno

---
+++ common/tasks.py
@@ -0,0 +1,11 @@
+import os
+import uuid
+
+class Task(object):
+    def __init__(self, task_exec, task_args, job_id, input_file, output_file, ):
+       input_file =
+
+
+if __name__ == '__main__':
+    new_task = Task()
+    print(new_task.name)
