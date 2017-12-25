0a11905f4c1d596608fb3a9e4aea0037836ea71b
LinLi <786487768@qq.com>
2017-12-24 19:43:47 -0800

    完善功能

--- common/executor.py
+++ common/executor.py
@@ -14,122 +14,116 @@ redis_instance = None
 
 
 def worker(task_queue):
-	while 1:
-		#time.sleep(1)
-		task_info = json.loads(task_queue.get())
-		# print(task_info.get('command'))
-		args = ["bash", "-c", "-l", task_info.get('command')]
-		p = Popen(args, stdout=PIPE, stderr=PIPE)
-		output, error = p.communicate()
-		print(output)
-		task_queue.task_done()
-		if task_queue.empty():
-			print("worker stop")
-			return
+    while 1:
+        # time.sleep(1)
+        task_info = json.loads(task_queue.get())
+        # print(task_info.get('command'))
+        args = ["bash", "-c", "-l", task_info.get('command')]
+        p = Popen(args, stdout=PIPE, stderr=PIPE)
+        output, error = p.communicate()
+        print(output)
+        task_queue.task_done()
+        if task_queue.empty():
+            print("worker stop")
+            return
 
 
 class Executor(object):
-	"""
-	任务执行器
+    """
+    任务执行器
 
-	从任务池中按序获取任务执行，并监控计算节点的信息和
-	任务执行时的资源使用情况
-	"""
-	def __init__(self, process_num=None, host=None, port=None):
-		if not process_num:
-			process_num = psutil.cpu_count()
-		if not host:
-			host = os.getenv("TASK_POOl_HOST")
-		if not port:
-			port = os.getenv("TASK_POOL_HOST")
-		self.process_num = process_num
-		self.redis_instance = redis.Redis(host=host, port=port)
+    从任务池中按序获取任务执行，并监控计算节点的信息和
+    任务执行时的资源使用情况
+    """
+    def __init__(self, process_num=None, host=None, port=None):
+        if not process_num:
+            process_num = psutil.cpu_count()
+        if not host:
+            host = os.getenv("TASK_POOl_HOST")
+        if not port:
+            port = os.getenv("TASK_POOL_HOST")
+        self.process_num = process_num
+        self.redis_instance = redis.Redis(host=host, port=port)
 
-	def _producer(self, task_queue):
-		while 1:
-			if task_queue.qsize() < self.process_num / 2:
-				for _ in range(self.process_num * 2):
-					task_info = self.redis_instance.lpop('task_info_list')
-					if task_info is None:
-						return
-					task_queue.put(task_info.decode('UTF-8'))
-			time.sleep(1)
+    def _producer(self, task_queue):
+        while 1:
+            if task_queue.qsize() < self.process_num / 2:
+                for _ in range(self.process_num * 2):
+                    task_info = self.redis_instance.lpop('task_info_list')
+                    if task_info is None:
+                        return
+                    task_queue.put(task_info.decode('UTF-8'))
+            time.sleep(1)
 
-	def _process_task(self):
-		task_queue = multiprocessing.Manager().Queue()
-		producer = multiprocessing.Process(target=self._producer, args=(task_queue,))
-		p = multiprocessing.Pool(self.process_num)
-		for i in range(self.process_num):
-			p.apply_async(worker, (task_queue,))
+    def _process_task(self):
+        task_queue = multiprocessing.Manager().Queue()
+        producer = multiprocessing.Process(target=self._producer, args=(task_queue,))
+        p = multiprocessing.Pool(self.process_num)
+        for i in range(self.process_num):
+            p.apply_async(worker, (task_queue,))
 
-		producer.start()
-		producer.join()
-		p.close()
-		p.join()
+        producer.start()
+        producer.join()
+        p.close()
+        p.join()
 
-		print("worker over")
-		return
+        print("worker over")
+        return
 
-	'''
-	def _oversee_resource(self, pids, interval):
-		def overseer(pid):
-			oversee_process = psutil.Process(pid=pid)
-			while 1:
-				print(oversee_process.cpu_percent(1))
-		threads = []
+    '''
+    def _oversee_resource(self, pids, interval):
+        def overseer(pid):
+            oversee_process = psutil.Process(pid=pid)
+            while 1:
+                print(oversee_process.cpu_percent(1))
+        threads = []
 
-		while 1:
-			pid = pids.get()
-			t = threading.Thread(target=overseer, args=(pid,))
-			t.start()
-			threads.append(t)
-			if pids.empty():
-				break
+        while 1:
+            pid = pids.get()
+            t = threading.Thread(target=overseer, args=(pid,))
+            t.start()
+            threads.append(t)
+            if pids.empty():
+                break
 
-		for t in threads:
-			t.join()
-	'''
-	def _oversee_resource(self, interval, process_task_t):
-		import socket
-		hostname = socket.gethostname()
-		dir = os.getcwd()
-		path = os.path.join(dir, hostname)
-		with open(path, 'a') as resource_info:
-			cpu_percents = []
-			timer = 0
-			while 1:
-				cpu_percents.append(str(psutil.cpu_percent()))
-				timer += 1
-				print(process_task_t.isAlive())
-				if not process_task_t.isAlive() or timer == 10:
-					resource_info.write('\n'.join(cpu_percents))
-					cpu_percents.clear()
-					timer = 0
-					if not process_task_t.isAlive():
-						return
-				time.sleep(1)
+        for t in threads:
+            t.join()
+    '''
+    def _oversee_resource(self, process_task_t, interval=1):
+        import socket
+        hostname = socket.gethostname()
+        current_dir = os.getcwd()
+        path = os.path.join(current_dir, hostname)
+        with open(path, 'a') as resource_info:
+            cpu_percents = []
+            timer = 0
+            while 1:
+                cpu_percents.append(str(psutil.cpu_percent()))
+                timer += 1
+                print(process_task_t.isAlive())
+                if not process_task_t.isAlive() or timer == 10:
+                    resource_info.write('\n'.join(cpu_percents))
+                    cpu_percents.clear()
+                    timer = 0
+                    if not process_task_t.isAlive():
+                        return
+                time.sleep(interval)
 
-	def run(self, interval=1):
-		process_task_t = threading.Thread(target=self._process_task,
-									name="ProcessTaskThread")
-		oversee_t = threading.Thread(target=self._oversee_resource,
-									 args=(interval,process_task_t,), name="OverseeThread")
+    def run(self, interval=1):
+        process_task_t = threading.Thread(target=self._process_task,
+                                          name="ProcessTaskThread")
+        oversee_t = threading.Thread(target=self._oversee_resource,
+                                     args=(process_task_t, interval,), name="OverseeThread")
 
-		process_task_t.start()
-		oversee_t.start()
-		process_task_t.join()
-		oversee_t.join()
+        process_task_t.start()
+        oversee_t.start()
+        process_task_t.join()
+        oversee_t.join()
 
 
 if __name__ == '__main__':
-	process_nums = int(sys.argv[1])
-	redis_host = sys.argv[2]
-	redis_port = sys.argv[3]
-	e = Executor(process_nums, 'localhost', 6379)
-	e.run()
-
-
-
-
-
-
+    process_nums = int(sys.argv[1])
+    redis_host = sys.argv[2]
+    redis_port = sys.argv[3]
+    e = Executor(process_nums, 'localhost', 6379)
+    e.run()
